11408d15387d45a00e75281d1653db30ff8fa295
asdribelb <asdribelb@gmail.com>
2023-09-16 13:20:23 -0300

    agregando try catch para manejar errores

--- src/router/carts.routes.js
+++ src/router/carts.routes.js
@@ -1,27 +1,58 @@
-import {Router} from "express"
+import { Router } from "express";
 import CartManager from "../controllers/CartManager.js";
 
-const CartRouter = Router()
-const carts = new CartManager
+const CartRouter = Router();
+const carts = new CartManager();
 
 CartRouter.post("/", async (req, res) => {
-    res.send(await carts.addCarts())
-
-})
-
-CartRouter.get('/', async (req,res) => {
-    res.send (await carts.readCarts()) 
-})
-
-CartRouter.get('/:id', async (req,res) => {
-    res.send (await carts.getCartsById(req.params.id)) 
-})
-
-CartRouter.post('/:carid/products/:pid', async (req, res) => {
-    let cartId = req.params.carid
-    let productId = req.params.pid
-    res.send (await carts.addProductInCart(cartId, productId))
-})
-
-
-export default CartRouter
+    try {
+      const result = await carts.addCarts();
+      res.status(200).json({ message: result });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Error interno del servidor' });
+    }
+  });
+  
+  CartRouter.get('/', async (req, res) => {
+    try {
+      const cartList = await carts.readCarts();
+      res.status(200).json(cartList);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Error interno del servidor' });
+    }
+  });
+  
+  CartRouter.get('/:id', async (req, res) => {
+    try {
+      const id = req.params.id;
+      const cartsById = await carts.getCartsById(id);
+      if (cartsById === "Carrito No Encontrado") {
+        res.status(404).json({ error: "Carrito No Encontrado" });
+      } else {
+        res.status(200).json(cartsById);
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Error interno del servidor' });
+    }
+  });
+  
+  CartRouter.post('/:carid/products/:pid', async (req, res) => {
+    try {
+      const cartId = req.params.carid;
+      const productId = req.params.pid;
+      const result = await carts.addProductInCart(cartId, productId);
+      if (result === "Carrito No Encontrado" || result === "Producto No Encontrado") {
+        res.status(404).json({ error: result });
+      } else {
+        res.status(200).json({ message: result });
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Error interno del servidor' });
+    }
+  });
+  
+  export default CartRouter;
--- src/router/product.routes.js
+++ src/router/product.routes.js
@@ -1,35 +1,74 @@
-import {Router} from "express"
+import { Router } from "express";
 import ProductManager from "../controllers/ProductManager.js";
+import ValidationError from "./ValidationError.js"
 
-const ProductRo = Router()
+const ProductRo = Router();
 const product = new ProductManager();
 
-
-
 ProductRo.get("/", async (req, res) => {
-    res.send(await product.getProducts())
+  try {
+    const products = await product.getProducts();
+    res.status(200).send(products);
+  } catch (error) {
+    // Maneja el error y devuelve un código de estado HTTP 500.
+    res.status(500).send("Error interno del servidor");
+  }
 });
 
 ProductRo.get("/:id", async (req, res) => {
-    let id = req.params.id
-    res.send(await product.getProductsById(id))
+  try {
+    const id = req.params.id;
+    const productById = await product.getProductsById(id);
+    if (!productById) {
+      // Si el producto no se encontró, devuelve un código de estado 404.
+      res.status(404).send("Producto no encontrado2");
+    } else {
+      res.status(200).send(productById);
+    }
+  } catch (error) {
+    // Maneja el error y devuelve un código de estado HTTP 500.
+    res.status(500).send("Error interno del servidor");
+  }
 });
 
 ProductRo.post("/", async (req, res) => {
-    let newProduct = req.body
-    res.send(await product.addProducts(newProduct))
+  try {
+    const newProduct = req.body;
+    const result = await product.addProducts(newProduct);
+    res.status(200).send(result);
+  } catch (error) {
+    // Maneja el error y devuelve un código de estado HTTP 400 o 500 según corresponda.
+    if (error instanceof ValidationError) {
+        console.error("Error de validación:", error.message);
+      res.status(400).send("Error de validación");
+    } else {
+        console.error("Error interno del servidor:", error.message);
+      res.status(500).send("Error interno del servidor");
+    }
+  }
 });
 
-ProductRo.put("/:id", async (req,res) => {
-    let id = req.params.id
-let updateProducts = req.body;
-res.send(await product.updateProducts(id, updateProducts))
-
+ProductRo.put("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const updateProducts = req.body;
+    const result = await product.updateProducts(id, updateProducts);
+    res.status(200).send(result);
+  } catch (error) {
+    // Maneja el error y devuelve un código de estado HTTP 500.
+    res.status(500).send("Error interno del servidor");
+  }
 });
 
-ProductRo.delete("/:id", async (req,res) => {
-    let id = req.params.id
-    res.send(await product.deleteProducts(id))
+ProductRo.delete("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const result = await product.deleteProducts(id);
+    res.status(200).send(result);
+  } catch (error) {
+    // Maneja el error y devuelve un código de estado HTTP 500.
+    res.status(500).send("Error interno del servidor");
+  }
 });
 
-export default ProductRo
+export default ProductRo;
