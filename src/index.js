c4b0ae2ebddd3d3762f13cb5f11c196095828660
asdribelb <asdribelb@gmail.com>
2023-10-22 17:10:43 -0300

    terminando implementacion login

--- src/index.js
+++ src/index.js
@@ -7,19 +7,20 @@ import prodRouter from "./router/product.routes.js"
 import cartRouter from "./router/cart.routes.js"
 import ProductManager from "./controllers/ProductManager.js"
 import CartManager from "./controllers/CartManager.js"
-
+import userRouter from "./router/user.routes.js"
+import MongoStore from "connect-mongo"
+import session from 'express-session'
+import FileStore from 'session-file-store'
 
 const app = express();
 const PORT = 8080;
-
+const fileStorage = FileStore(session)
 const product = new ProductManager()
 const cart = new CartManager()
 
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: false }))
 
-app.use("/api/products", prodRouter)
-app.use("/api/carts", cartRouter)
 
 app.listen(PORT, () => {
     console.log(`Servidor Express Puerto ${PORT}`)
@@ -36,6 +37,21 @@ mongoose.connect("mongodb+srv://asdribelb:LIaLTms1Lcgdfohq@abellorin.mity2xr.mon
     })
 
 
+//Mongo Atlas
+app.use(session({
+    store: MongoStore.create({
+        mongoUrl: "mongodb+srv://asdribelb:LIaLTms1Lcgdfohq@abellorin.mity2xr.mongodb.net/?retryWrites=true&w=majority",
+        mongoOptions: { useNewUrlParser: true, useUnifiedTopology: true }, ttl: 3600
+    }),
+    secret: "ClaveSecreta",
+    resave: false,
+    saveUninitialized: false,
+}))
+
+app.use("/api/products", prodRouter)
+app.use("/api/carts", cartRouter)
+app.use("/api/sessions", userRouter)
+
 //Handlebars
 app.engine("handlebars", engine())
 app.set("view engine", "handlebars")
@@ -45,23 +61,54 @@ app.set("views", path.resolve(__dirname + "/views"))
 app.use(express.static(__dirname + "/public"));
 
 app.get("/products", async (req, res) => {
-    let allProducts  = await product.getProducts()
+    if (!req.session.emailUsuario) {
+        return res.redirect("/login")
+    }
+    let allProducts = await product.getProducts()
     allProducts = allProducts.map(product => product.toJSON());
     res.render("viewProducts", {
         title: "Vista Productos",
-        products : allProducts
+        products: allProducts,
+        email: req.session.emailUsuario,
+        rol: req.session.rolUsuario,
+        algo: req.session.algo,
     });
 })
 
 
 app.get("/carts/:cid", async (req, res) => {
     let id = req.params.cid
-    let allCarts  = await cart.getCartWithProducts(id)
+    let allCarts = await cart.getCartWithProducts(id)
     res.render("viewCart", {
         title: "Vista Carro",
-        carts : allCarts
+        carts: allCarts
     });
 })
 
+//Ingreso Login http://localhost:8080/login
+app.get("/login", async (req, res) => {
+    res.render("login", {
+        title: "Login",
+    });
 
+})
+//Ingreso Register http://localhost:8080/register
+app.get("/register", async (req, res) => {
+    res.render("register", {
+        title: "Register",
+    });
+})
+//Ingreso Profile http://localhost:8080/profile
+app.get("/profile", async (req, res) => {
+    if (!req.session.emailUsuario) {
+        return res.redirect("/login")
+    }
+    res.render("profile", {
+        title: "Profile Admin",
+        first_name: req.session.nomUsuario,
+        last_name: req.session.apeUsuario,
+        email: req.session.emailUsuario,
+        rol: req.session.rolUsuario,
 
+    });
+})
