54c1b06746aeb8de5479585adf6756fcdb7096c0
asdribelb <asdribelb@gmail.com>
2023-12-09 18:40:17 -0300

    agregando logging

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import 'dotenv/config'
-import app from './app.js';
-
-// Inicia la aplicación
-const PORT = 8080;
-app.listen(PORT, () => console.log('Servidor conectado en puerto 8080'));
---
+++ src/router/carts.router.js
@@ -0,0 +1,58 @@
+import { Router } from "express"
+
+import CartDTO from "../dao/DTOs/cart.dto.js";
+import TicketDTO from "../dao/DTOs/ticket.dto.js";
+import { ticketService, cartService } from "../repositories/index.js";
+import Carts from "../dao/mongo/carts.mongo.js";
+
+const router = Router()
+const cartMongo = new Carts()
+
+router.get("/", async (req, res) => {
+  let result = await cartMongo.get()
+  res.send({ status: "success", payload: result })
+})
+
+router.post("/", async (req, res) => {
+  let { products } = req.body
+
+  let cart = new CartDTO({ products })
+  let result = await cartService.createCart(cart)
+  if(result){
+      req.logger.info('Se crea carrito correctamente correctamente');
+  }else{
+      req.logger.error("Error al crear carritos");
+  }
+})
+
+router.post("/:cid/purchase", async (req, res) => {
+  try {
+      let id_cart = req.params.cid;
+  
+      const productos = req.body.productos;
+      const correo = req.body.correo;
+      console.log(correo)
+      let cart = cartService.validateCart(id_cart)
+      if (!cart) {
+        req.logger.error("No se encontró el carrito con el ID proporcionado");
+          return { error: "No se encontró el carrito con el ID proporcionado" };
+      }
+      let validaStock = cartService.validateStock({productos})
+
+      if (validaStock) {
+          let totalAmount = await cartService.getAmount({productos})
+          const ticketFormat = new TicketDTO({amount:totalAmount, purchaser:correo});
+          const result = await ticketService.createTicket(ticketFormat);
+      } else {
+        req.logger.error("No hay suficiente stock para realizar la compra");
+      }
+  } catch (error) {
+    req.logger.error("Error al procesar la compra:" + error.message);
+    console.error("Error al procesar la compra:", error);
+    return res.status(500).json({ error: "Error interno al procesar la compra" });
+  }
+})
+
+export default router
+
+
