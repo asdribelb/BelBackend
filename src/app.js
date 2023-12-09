52690aaa81c9eb210d5054b12dc601e76656c40b
asdribelb <asdribelb@gmail.com>
2023-12-09 18:41:35 -0300

    mocking 2.1

--- src/app.js
+++ src/app.js
@@ -1,16 +1,25 @@
 import express from "express";
 import { engine } from "express-handlebars";
 import * as path from "path"
-import __dirname from "./utils.js";
-import connectDB from './config/db.js';
-import userRouter from "./router/user.routes.js"
 import mongoose from 'mongoose'
+import config from './config/config.js'
 import passport from "passport"
-import session from 'express-session'
-import sessionConfig from './config/session.js';
+import cookieParser from "cookie-parser"
 import initializePassword from "./config/passport.config.js"
-import prodRouter from "./router/product.routes.js"
-import cartRouter from "./router/cart.routes.js"
+import {generateAndSetToken} from "./jwt/token.js"
+import cartsRouter from './router/carts.router.js'
+import productsRouter from './router/products.router.js'
+import usersRouter from './router/users.router.js'
+import ticketsRouter from './router/tickets.router.js'
+import UserMongo from "./dao/mongo/users.mongo.js"
+import ProdMongo from "./dao/mongo/products.mongo.js"
+import { Strategy as JwtStrategy } from 'passport-jwt';
+import { ExtractJwt as ExtractJwt } from 'passport-jwt';
+import {Server} from "socket.io"
+import __dirname, { authorization, passportCall, transport } from "./utils.js"
+import UserDTO from './dao/DTOs/user.dto.js'
+import compression from 'express-compression'
+import { nanoid } from 'nanoid'
 
 
 // Configuración de .env
@@ -18,26 +27,102 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
+const PORT = 8080;
+
+const users = new UserMongo()
+const products = new ProdMongo()
+
+mongoose.connect(config.mongo_url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+app.listen(PORT, () => {
+    console.log(`Servidor Express Puerto ${PORT}`)
+})
 
-// Conectar a la base de datos MongoDB
-connectDB();
+//Mongo Atlas
+app.use(session({
+    store: MongoStore.create({
+        mongoUrl: "mongodb+srv://asdribelb:LIaLTms1Lcgdfohq@abellorin.mity2xr.mongodb.net/?retryWrites=true&w=majority",
+        mongoOptions: { useNewUrlParser: true, useUnifiedTopology: true }, ttl: 3600
+    }),
+    secret: "ClaveSecreta",
+    resave: false,
+    saveUninitialized: false,
+}))
 
-// Configuración de la sesión
-app.use(session(sessionConfig));
+const socketServer = new Server(httpServer)
+
+socketServer.on("connection", socket => {
+    console.log("Socket Conectado")
+})
+
+    socket.on("message", data => {
+        console.log(data)
+    })
+
+    socket.on("newProd", (newProduct) => {
+        products.addProduct(newProduct)
+        socketServer.emit("success", "Producto Agregado Correctamente");
+    });
+    socket.on("updProd", ({id, newProduct}) => {
+        products.updateProduct(id, newProduct)
+        socketServer.emit("success", "Producto Actualizado Correctamente");
+    });
+    socket.on("delProd", (id) => {
+        products.deleteProduct(id)
+        socketServer.emit("success", "Producto Eliminado Correctamente");
+    });
+
+    socket.on("newEmail", async({email, comment}) => {
+        let result = await transport.sendMail({
+            from:'Chat Correo <asdribelb@gmail.com>',
+            to:email,
+            subject:'Mensaje con Socket',
+            html:`
+            <div>
+                <h1>${comment}</h1>
+            </div>
+            `,
+            attachments:[]
+        })
+        socketServer.emit("success", "Correo enviado correctamente");
+    });
+
+    socket.emit("test","mensaje desde servidor a cliente, se valida en consola de navegador")
+
+const jwtOptions = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: "Secret-key"
+}
+
+passport.use(
+    new JwtStrategy(jwtOptions, (jwt_payload, done)=>{
+        const user = users.findJWT((user) =>user.email ===jwt_payload.email)
+        if(!user)
+        {
+            return done(null, false, {message:"Usuario no encontrado"})
+        }
+        return done(null, user)
+    })
+)
+
 
 //Passport//
 initializePassword()
 app.use(passport.initialize())
-app.use(passport.session())
+app.use(compression());
+app.use(cookieParser());
 
 // Rutas para manejar productos, carritos y sesiones de usuario
-app.use("/api/products", prodRouter)
-app.use("/api/carts", cartRouter)
-app.use("/api/sessions", userRouter)
+app.use("/carts", cartsRouter)
+app.use("/products", productsRouter)
+app.use("/users", usersRouter)
+app.use("/tickets", ticketsRouter)
 
 //Handlebars
 app.engine("handlebars", engine())
@@ -47,6 +132,78 @@ app.set("views", path.resolve(__dirname + "/views"))
 //static
 app.use(express.static(__dirname + "/public"));
 
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+    const emailToFind = email;
+    const user = await users.findEmail({ email: emailToFind });
+    if (!user || user.password !== password) {
+      return res.status(401).json({ message: "Error de autenticación" });
+    }
+    const token = generateAndSetToken(res, email, password);
+    const userDTO = new UserDTO(user);
+    const prodAll = await products.get()
+    res.json({ token, user: userDTO, prodAll});
+  });
+
+app.post("/api/register", async(req,res)=>{
+    const {first_name, last_name, email,age, password, rol} = req.body
+    const emailToFind = email
+    const exists = await users.findEmail({ email: emailToFind })
+    if(exists) return res.status(400).send({status:"error", error: "Usuario ya existe"})
+    const newUser = {
+        first_name,
+        last_name,
+        email,
+        age,
+        password,
+        rol
+    };
+    users.addUser(newUser)
+    const token = generateAndSetToken(res, email, password) 
+    res.send({token}) 
+})
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: app.get('views') });
+});
+app.get('/register', (req, res) => {
+    res.sendFile('register.html', { root: app.get('views') });
+});
+app.get('/current',passportCall('jwt', { session: false }), authorization('user'),(req,res) =>{
+    authorization('user')(req, res,async() => {      
+        const prodAll = await products.get();
+        res.render('home', { products: prodAll });
+    });
+})
+app.get('/admin',passportCall('jwt'), authorization('user'),(req,res) =>{
+    authorization('user')(req, res,async() => {    
+        const prodAll = await products.get();
+        res.render('admin', { products: prodAll });
+    });
+})
+
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+app.get("/mockingproducts", async(req,res)=>{
+
+    const products = [];
+
+    for (let i = 0; i < 50; i++) {
+        const product = {
+            id: nanoid(),
+            description: `Apple ${i + 1}`,
+            image: 'https://apple.com/image.jpg',
+            price: getRandomNumber(1, 1000),
+            stock: getRandomNumber(1, 100),
+            category: `celulares ${i % 5 + 1}`,
+            availability: 'in_stock'
+        };
+
+        products.push(product);
+    }
+
+    res.send(products);
+})
 
 export default app;
-
--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,36 @@
 import path from "path"
 import { fileURLToPath } from "url"
-import bcrypt from "bcrypt"
+import passport from "passport"
+import nodemailer from 'nodemailer'
+import bcrypt from 'bcrypt'
 
-export const createHash = password => bcrypt.hashSync(password, bcrypt.genSaltSync(10))
-export const isValidPassword = (user, password) => bcrypt.compareSync(password, user.password)
+export const passportCall = (strategy) => {
+    return async(req, res, next)=>{
+        passport.authenticate(strategy, function(err, user, info){
+            if(err) return next(err)
+            if(!user){
+                return res.status(401).send({error:info.messages?info.messages:info.toString()})
+            }
+            req.user = user
+            next()
+        })(req, res, next)
+    }
+}
+export const authorization= (role) => {
+    return async(req, res, next)=>{
+        if(!req.user) return res.status(401).send({error: "Unauthorized"})
+        if(req.user.role!= role) return res.status(403).send({error:"No permissions"})
+        next()
+    }
+}
+export const transport= nodemailer.createTransport({
+    service:'gmail',
+    port:587,
+    auth:{
+        user:'asdribelb@gmail.com',
+        pass:'fyeb yqwu hblz izxy'
+    }
+})
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
