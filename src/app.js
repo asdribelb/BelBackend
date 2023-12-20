e469e0901592116b1161bd3a052c00a8de1fdada
asdribelb <asdribelb@gmail.com>
2023-12-19 22:12:36 -0300

    probando el index2

--- src/app.js
+++ src/app.js
@@ -14,13 +14,13 @@ import UserMongo from "./dao/mongo/users.mongo.js"
 import ProdMongo from "./dao/mongo/products.mongo.js"
 import { Strategy as JwtStrategy } from 'passport-jwt';
 import { ExtractJwt as ExtractJwt } from 'passport-jwt';
-import {Server} from "socket.io"
+import { Server } from "socket.io"
 import __dirname, { authorization, passportCall, transport } from "./utils.js"
-import {generateAndSetToken} from "./jwt/token.js"
+import { generateAndSetToken } from "./jwt/token.js"
 import UserDTO from './dao/DTOs/user.dto.js'
 import session from "express-session";
 import MongoStore from "connect-mongo";
-import http from 'http';
+import { createServer } from "http";
 
 
 // Configuración de .env
@@ -28,9 +28,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
 const PORT = 8080;
-const server = http.createServer(app)
-const io = new Server(server)
 
 const users = new UserMongo()
 const products = new ProdMongo()
@@ -40,12 +40,24 @@ mongoose.connect(config.MONGO_URL, {
     useUnifiedTopology: true,
 });
 
+const jwtOptions = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: "Secret-key"
+}
+
+passport.use(
+    new JwtStrategy(jwtOptions, (jwt_payload, done) => {
+        const user = users.findJWT((user) => user.email === jwt_payload.email)
+        if (!user) {
+            return done(null, false, { message: "Usuario no encontrado" })
+        }
+        return done(null, user)
+    })
+)
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-server.listen(PORT, ()=>{
-    console.log(`Servidor corriendo en puerto ${PORT}`)
-  });
 
 //Mongo Atlas
 app.use(session({
@@ -58,48 +70,37 @@ app.use(session({
     saveUninitialized: false,
 }))
 
-io.on('connection', (socket) => {
-    console.log('Cliente conectado');
-  
-    io.emit('conexion-establecida', 'Conexión exitosa con el servidor de Socket');
-    io.on('disconnect', () => {
-      console.log('Cliente desconectado');
-    });
-  });
+io.on("connection", (socket) => {
+    console.log(`Cliente conectado ${socket.id}`)
 
-    io.on("newEmail", async({email, comment}) => {
+    socket.emit('conexion-establecida', 'Conexión exitosa con el servidor de Socket');
+
+    socket.on('disconnect', () => {
+        console.log('Cliente desconectado');
+    });
+
+    socket.on("newEmail", async ({ email, comment }) => {
         let result = await transport.sendMail({
-            from:'Chat Correo <asdribelb@gmail.com>',
-            to:email,
-            subject:'Mensaje con Socket',
-            html:`
+            from: 'Chat Correo <asdribelb@gmail.com>',
+            to: email,
+            subject: 'Mensaje con Socket',
+            html: `
             <div>
                 <h1>${comment}</h1>
             </div>
             `,
-            attachments:[]
+            attachments: []
         })
-        socket.emit("success", "Correo enviado correctamente");
+        io.emit("success", "Correo enviado correctamente");
     });
 
-    io.emit("test","mensaje desde servidor a cliente, se valida en consola de navegador")
+    socket.emit("test", "mensaje desde servidor a cliente, se valida en consola de navegador")
+});
 
-const jwtOptions = {
-    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    secretOrKey: "Secret-key"
-}
-
-passport.use(
-    new JwtStrategy(jwtOptions, (jwt_payload, done)=>{
-        const user = users.findJWT((user) =>user.email ===jwt_payload.email)
-        if(!user)
-        {
-            return done(null, false, {message:"Usuario no encontrado"})
-        }
-        return done(null, user)
-    })
-)
-
+// Iniciar el servidor después de todas las configuraciones
+httpServer.listen(PORT, () => {
+    console.log(`Servidor Express Puerto: ${PORT}`);
+});;
 
 //Passport//
 initializePassword()
@@ -124,19 +125,19 @@ app.post("/login", async (req, res) => {
     const emailToFind = email;
     const user = await users.findEmail({ email: emailToFind });
     if (!user || user.password !== password) {
-      return res.status(401).json({ message: "Error de autenticación" });
+        return res.status(401).json({ message: "Error de autenticación" });
     }
     const token = generateAndSetToken(res, email, password);
     const userDTO = new UserDTO(user);
     const prodAll = await products.get()
-    res.json({ token, user: userDTO, prodAll});
-  });
+    res.json({ token, user: userDTO, prodAll });
+});
 
-app.post("/api/register", async(req,res)=>{
-    const {first_name, last_name, email,age, password, rol} = req.body
+app.post("/api/register", async (req, res) => {
+    const { first_name, last_name, email, age, password, rol } = req.body
     const emailToFind = email
     const exists = await users.findEmail({ email: emailToFind })
-    if(exists) return res.status(400).send({status:"error", error: "Usuario ya existe"})
+    if (exists) return res.status(400).send({ status: "error", error: "Usuario ya existe" })
     const newUser = {
         first_name,
         last_name,
@@ -146,8 +147,8 @@ app.post("/api/register", async(req,res)=>{
         rol
     };
     users.addUser(newUser)
-    const token = generateAndSetToken(res, email, password) 
-    res.send({token}) 
+    const token = generateAndSetToken(res, email, password)
+    res.send({ token })
 })
 app.get('/', (req, res) => {
     res.sendFile('index.html', { root: app.get('views') });
@@ -155,14 +156,14 @@ app.get('/', (req, res) => {
 app.get('/register', (req, res) => {
     res.sendFile('register.html', { root: app.get('views') });
 });
-app.get('/current',passportCall('jwt', { session: false }), authorization('user'),(req,res) =>{
-    authorization('user')(req, res,async() => {      
+app.get('/current', passportCall('jwt', { session: false }), authorization('user'), (req, res) => {
+    authorization('user')(req, res, async () => {
         const prodAll = await products.get();
         res.render('home', { products: prodAll });
     });
 })
-app.get('/admin',passportCall('jwt'), authorization('user'),(req,res) =>{
-    authorization('user')(req, res,async() => {    
+app.get('/admin', passportCall('jwt'), authorization('user'), (req, res) => {
+    authorization('user')(req, res, async () => {
         const prodAll = await products.get();
         res.render('admin', { products: prodAll });
     });
